304238dd781789e8f1d4b27a2aed07744ddc7fe9
zackjj <zack.j.johnston@gmail.com>
2021-04-26 18:48:25 -0700

    added function to write inquirer response to README.md

---
+++ README.md
@@ -0,0 +1,31 @@
+
+# Real Project
+[![GitHub License](https://img.shields.io/badge/license-MIT-green)](License.md)
+## Description
+[object Object]
+## Table of Contents
+[Installation](#installation)  
+[Usage](#usage)  
+[Contribution](#contribution)  
+[Testing](#testing)  
+[Questions](#questions)  
+## Installation
+Install programs, then run
+<br/>
+## Usage
+Use it do a project
+<br/>
+## Contribution
+Please contribute to my project
+<br/>
+## Testing
+For testing, run the following command:
+Run a test
+<br/>
+## License
+MIT 
+<br/>
+## Questions
+Please contact the author with questions via:
+* [zack.j.johnston@gmail.com](mailto:zack.j.johnston@gmail.com)
+* [zackjjohnston](https://github.com/zackjjohnston)
--- index.js
+++ index.js
@@ -1,75 +1,103 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 //Create an array of questions for user input
-const questions = [
-    {    
-        type: 'input',
-        message: 'What is the title of your project?',
-        name: 'title'
-    },
-    {
-        type: 'input',
-        message: 'Please provide a description of your project',
-        name: 'description.'
-    },
-    {
-        type: 'input',
-        message: 'Please provide a installation instructions for your project.',
-        name: 'installation'
-    },
-    {
-        type: 'input',
-        message: 'Please provide a decription of usage of the project.',
-        name: 'usage'
-    },
-    {
-        type: 'input',
-        message: 'Please provide the contribution guidelines for your project.',
-        name: "contribution"
-    },
-    {
-        type: 'input',
-        message: 'Please provide instructions on how the project should be tested.',
-        name: "instructions"
-    },
-    {
-        type: 'input',
-        message: 'Please provide the license used for the project.',
-        name: 'list'
-    },
-    {
-        type: 'input',
-        message: 'Please provide your GitHub username.',
-        name: 'username'
-    },    
-    {
-        type: 'input',
-        message: 'Please provide your e-mail address.',
-        name: 'email'
-    }
-]
-
-// TODO: Create a function to write README file
-{
-    promptUser().then((answers) => {
-        try {
-            const readme = generateREADME(answers);
-            fs.writeFileSync('README.md', readme);
-            console.log('Succesfully wrote README.md file');
-        } catch (error) {
-            console.log(error);
+const userPrompt = () => 
+    inquirer.prompt([
+        {    
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'Please provide a description of your project',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'Please provide a installation instructions for your project.',
+            name: 'installation'
+        },
+        {
+            type: 'input',
+            message: 'Please provide a decription of usage of the project.',
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message: 'Please provide the contribution guidelines for your project.',
+            name: "contribution"
+        },
+        {
+            type: 'input',
+            message: 'Please provide instructions on how the project should be tested.',
+            name: "testing"
+        },
+        {
+            type: 'input',
+            message: 'What license was used for the project?',
+            name: 'license'
+        },
+        {
+            type: 'input',
+            message: 'Please provide your GitHub username.',
+            name: 'username'
+        },    
+        {
+            type: 'input',
+            message: 'Please provide your e-mail address.',
+            name: 'email'
         }
-    });
-};
+    ])
 
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions).then((response) => { 
-        fs.writeFileSync(`${response.title}-README.md`, writeToFile(response), (err) => { 
-            err ? console.error(err) : console.log('Your new README file has been generated!')
-        })
+//Create a function to write README file
+const writeREADME = (answers) => {
+return `
+# ${answers.title}
+[![GitHub License](https://img.shields.io/badge/license-${answers.license}-green)](License.md)
+## Description
+${answers.description}
+## Table of Contents
+[Installation](#installation)  
+[Usage](#usage)  
+[Contribution](#contribution)  
+[Testing](#testing)  
+[Questions](#questions)  
+## Installation
+${answers.installation}
+<br/>
+## Usage
+${answers.usage}
+<br/>
+## Contribution
+${answers.contribution}
+<br/>
+## Testing
+For testing, run the following command:
+${answers.testing}
+<br/>
+## License
+${answers.license} 
+<br/>
+## Questions
+Please contact the author with questions via:
+* [${answers.email}](mailto:${answers.email})
+* [${answers.username}](https://github.com/${answers.username})
+`
+}
+
+//Create a function to initialize app
+function init () {
+    userPrompt().then((answers) => {
+        try {
+            const readme = writeREADME(answers)
+            fs.writeFileSync('README.md', readme)
+            console.log('README.md has been saved')
+        } 
+        catch (error) {
+            console.log(error)
+        }
     })
- }
-
+}
 // Function call to initialize app
-init();
+init()
